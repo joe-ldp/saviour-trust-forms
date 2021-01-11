593e687c752f898c0fb3aded82661939c43667a5
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-01-11 11:31:59 +0000

    Project creation

---
+++ GSDP/GSDP/GSDP/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using GSDP.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace GSDP.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ GSDP/GSDP/GSDP/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using GSDP.Models;
+using GSDP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GSDP.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
