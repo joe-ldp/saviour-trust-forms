746eb24c004ad0fd1c6dda91ecf6f0c8d47827d8
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-01-13 16:19:29 +0000

    form update

--- SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/SecondFragment.java
+++ SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/SecondFragment.java
@@ -1,34 +1,65 @@
 package com.saviourtrust.trustforms;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
+public class SecondFragment extends Fragment
+{
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+            Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false);
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
+    {
         super.onViewCreated(view, savedInstanceState);
 
-        /*view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        /*view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });*/
+
+        EditText textDate = (EditText) view.findViewById(R.id.editTextDate);
+        EditText textTime = (EditText) view.findViewById(R.id.editTextTime);
+
+        String date_n = new SimpleDateFormat("dd/mm/yyyy", Locale.getDefault()).format(new Date());
+
+        Calendar c = Calendar.getInstance();
+        int hr = c.get(Calendar.HOUR_OF_DAY);
+        int mm = c.get(Calendar.MINUTE);
+        StringBuilder time_n = new StringBuilder();
+        time_n.append(hr);
+        time_n.append(":");
+        time_n.append(mm);
+
+        textDate.setText(date_n);
+        textTime.setText(time_n);
+
+
     }
 }
