83873974f40f61e8dd6440d594b3c2d1046767e8
omerBoio <omerhyman@gmail.com>
2021-01-14 17:43:43 +0000

    Revert "form update"
    This reverts commit 9dc3be7c2418efbaed1b58f75f1939459bb3c207.

--- SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/SecondFragment.java
+++ SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/SecondFragment.java
@@ -1,18 +1,22 @@
 package com.saviourtrust.trustforms;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
+import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class SecondFragment extends Fragment
@@ -56,90 +60,6 @@ public class SecondFragment extends Fragment
         textDate.setText(date_n);
         textTime.setText(time_n);
 
-        CheckBox cbSmokeAlarms = view.findViewById(R.id.checkBoxSmokeAlarms);
-        EditText etSmokeAlarms = view.findViewById(R.id.editTextSmokeAlarms);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbSmokeAlarms.isChecked(), etSmokeAlarms);
-            }
-        });
 
-        CheckBox cbCarbonMonoxideAlarms = view.findViewById(R.id.checkBoxCarbonMonoxideAlarms);
-        EditText etCarbonMonoxideAlarms = view.findViewById(R.id.editTextCarbonMonoxideAlarms);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbCarbonMonoxideAlarms.isChecked(), etCarbonMonoxideAlarms);
-            }
-        });
-
-        CheckBox cbFridgeFreezer = view.findViewById(R.id.checkBoxFridgeFreezer);
-        EditText etFridgeFreezer = view.findViewById(R.id.editTextFridgeFreezer);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbFridgeFreezer.isChecked(), etFridgeFreezer);
-            }
-        });
-
-        CheckBox cbWasher = view.findViewById(R.id.checkBoxWasher);
-        EditText etWasher = view.findViewById(R.id.editTextWasher);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbWasher.isChecked(), etWasher);
-            }
-        });
-
-        CheckBox cbMicrowave = view.findViewById(R.id.checkBoxMicrowave);
-        EditText etMicrowave = view.findViewById(R.id.editTextMicrowave);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbMicrowave.isChecked(), etMicrowave);
-            }
-        });
-
-        CheckBox cbKettle = view.findViewById(R.id.checkBoxKettle);
-        EditText etKettle = view.findViewById(R.id.editTextKettle);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbKettle.isChecked(), etKettle);
-            }
-        });
-
-        CheckBox cbHoover = view.findViewById(R.id.checkBoxHoover);
-        EditText etHoover = view.findViewById(R.id.editTextHoover);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbHoover.isChecked(), etHoover);
-            }
-        });
-
-        CheckBox cbFryer = view.findViewById(R.id.checkBoxFryer);
-        EditText etFryer = view.findViewById(R.id.editTextFryer);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbFryer.isChecked(), etFryer);
-            }
-        });
-
-        CheckBox cbToaster = view.findViewById(R.id.checkBoxToaster);
-        EditText etToaster = view.findViewById(R.id.editTextToaster);
-        cbSmokeAlarms.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                doVisibility(cbToaster.isChecked(), etToaster);
-            }
-        });
-
-
-    }
-
-    public void doVisibility(boolean checked, EditText et)
-    {
-        if (checked)
-        {
-            et.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            et.setVisibility(View.INVISIBLE);
-        }
     }
 }
