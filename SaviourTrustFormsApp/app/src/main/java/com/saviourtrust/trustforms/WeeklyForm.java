7c917fc262feb5330b307660ea99e9ed5ceb0ce3
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-01-18 17:10:38 +0000

    add all form files, update gitignore

--- SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/WeeklyForm.java
+++ SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/WeeklyForm.java
@@ -16,7 +16,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class SecondFragment extends Fragment
+public class WeeklyForm extends Fragment
 {
     @Override
     public View onCreateView(
@@ -24,7 +24,7 @@ public class SecondFragment extends Fragment
             Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_weekly_form, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
@@ -76,7 +76,7 @@ public class SecondFragment extends Fragment
         view.findViewById(R.id.btnSubmit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                NavHostFragment.findNavController(SecondFragment.this)
+                NavHostFragment.findNavController(WeeklyForm.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
 
 
