8313fa6f146bb097af6408051c098b7496da93ae
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-02-16 10:46:27 +0000

    update property form (basically done)

--- SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/PropertyForm.java
+++ SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/PropertyForm.java
@@ -2,11 +2,14 @@ package com.saviourtrust.trustforms;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +63,12 @@ public class PropertyForm extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_property_form, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
