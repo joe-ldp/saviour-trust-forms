aaeb20ad32fffbdb20c0d73c4bc39ac5630dbbe9
Joe <67245465+joe-ldp@users.noreply.github.com>
2021-02-17 14:25:38 +0000

    update form a bit, capture input from form in variables

--- SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/PropertyForm.java
+++ SaviourTrustFormsApp/app/src/main/java/com/saviourtrust/trustforms/PropertyForm.java
@@ -1,5 +1,6 @@
 package com.saviourtrust.trustforms;
 
+import android.location.Address;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,60 +9,74 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PropertyForm#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Date;
+
 public class PropertyForm extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText editTextAddress;
+    Address address;
+    String addressString;
+    EditText editTextPostcode;
+    String postcode;
+    EditText editTextOwnerName;
+    String ownerName;
+    EditText editTextOwnerPhone;
+    String ownerPhone;
+    EditText editTextRentPaid;
+    double rentPaid;
+    String rentPaidString;
+    EditText editTextRoomRate;
+    double roomRate;
+    String roomRateString;
+    EditText editTextNumRooms;
+    int numRooms;
+    String numRoomsString;
+    EditText editTextNumVoids;
+    int numVoids;
+    String numVoidsString;
+    EditText editTextDate;
+    Date date;
+    String dateString;
+    EditText editTextElectCertExpiry;
+    Date electCertExpiry;
+    String electCertExpiryString;
+    EditText editTextElectSerialNo;
+    String electSerialNo;
+    EditText editTextElectProvider;
+    String electProvider;
+    EditText editTextElectMeter;
+    String electMeter;
+    EditText editTextGasCertExpiry;
+    Date gasCertExpiry;
+    String gasCertExpiryString;
+    EditText editTextGasSerialNo;
+    String gasSerialNo;
+    EditText editTextGasProvider;
+    String gasProvider;
+    EditText editTextGasMeter;
+    String gasMeter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public PropertyForm() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PropertyForm.
-     */
     // TODO: Rename and change types and number of parameters
     public static PropertyForm newInstance(String param1, String param2) {
         PropertyForm fragment = new PropertyForm();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_property_form, container, false);
     }
 
@@ -69,6 +84,53 @@ public class PropertyForm extends Fragment {
     {
         super.onViewCreated(view, savedInstanceState);
 
+        editTextAddress = (EditText) view.findViewById(R.id.editTextAddress);
+        editTextPostcode = (EditText) view.findViewById(R.id.editTextPostcode);
+        editTextOwnerName = (EditText) view.findViewById(R.id.editTextOwnerName);
+        editTextOwnerPhone = (EditText) view.findViewById(R.id.editTextOwnerPhone);
+        editTextRentPaid = (EditText) view.findViewById(R.id.editTextRentPaid);
+        editTextRoomRate = (EditText) view.findViewById(R.id.editTextRoomRate);
+        editTextNumRooms = (EditText) view.findViewById(R.id.editTextNumRooms);
+        editTextNumVoids = (EditText) view.findViewById(R.id.editTextNumVoids);
+        editTextDate = (EditText) view.findViewById(R.id.editTextDate);
+        editTextElectCertExpiry = (EditText) view.findViewById(R.id.editTextElectCertExpiry);
+        editTextElectSerialNo = (EditText) view.findViewById(R.id.editTextElectSerialNo);
+        editTextElectProvider = (EditText) view.findViewById(R.id.editTextElectProvider);
+        editTextElectMeter = (EditText) view.findViewById(R.id.editTextElectMeter);
+        editTextGasCertExpiry = (EditText) view.findViewById(R.id.editTextGasCertExpiry);
+        editTextGasSerialNo = (EditText) view.findViewById(R.id.editTextGasSerialNo);
+        editTextGasProvider = (EditText) view.findViewById(R.id.editTextGasProvider);
+        editTextGasMeter = (EditText) view.findViewById(R.id.editTextGasMeter);
 
+        view.findViewById(R.id.btnSubmit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addressString = editTextAddress.getText().toString();
+                //address = .getText().toString();
+                postcode = editTextPostcode.getText().toString();
+                ownerName = editTextOwnerName.getText().toString();
+                ownerPhone = editTextOwnerPhone.getText().toString();
+                rentPaidString = editTextRentPaid.getText().toString();
+                rentPaid = Double.parseDouble(rentPaidString);
+                roomRateString = editTextRoomRate.getText().toString();
+                roomRate = Double.parseDouble(roomRateString);
+                numRoomsString = editTextNumRooms.getText().toString();
+                numRooms = Integer.parseInt(numRoomsString);
+                numVoidsString = editTextNumVoids.getText().toString();
+                numVoids = Integer.parseInt(numVoidsString);
+                dateString = editTextDate.getText().toString();
+                //date = dateString;
+                electCertExpiryString = editTextElectCertExpiry.getText().toString();
+                //electCertExpiry = electCertExpiryString;
+                electSerialNo = editTextElectSerialNo.getText().toString();
+                electProvider = editTextElectProvider.getText().toString();
+                electMeter = editTextElectMeter.getText().toString();
+                gasCertExpiryString = editTextGasCertExpiry.getText().toString();
+                //gasCertExpiry = gasCertExpiryString;
+                gasSerialNo = editTextGasSerialNo.getText().toString();
+                gasProvider = editTextGasProvider.getText().toString();
+                gasMeter = editTextGasMeter.getText().toString();
+            }
+        });
     }
 }
